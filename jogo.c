5b0a949d97bb1b1357005ffc54689d13fe685d84
Jean Daniel da Silva Santos <jeandaniel.contato@gmail.com>
2026-06-06 15:48:51 -0300

    Menu inicial com o while e .gitignore.
    Adicionei o while para o menu ficar repetindo ate o usuário informar que quer sair do programa e criei o arquivo .gitignore para não ficar muito bagunçado com os arquivos executáveis que o compilador cria.

--- jogo.c
+++ jogo.c
@@ -1,37 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void tela_inicial();
+void pausa();
+
+int opcao;
 
 int main()
 {
-    int opcao;
 
+    do { 
+
+        tela_inicial();
+
+        switch(opcao)
+        {
+            case 1:
+                printf("Iniciando...\n");
+                break;
+
+            case 2:
+                printf("Sao 18 perguntas no total e a cada resposta certa voce avanca 1 nivel\n"
+                       "ao responder todas de forma correta, voce vence o QUIZ Com Niveis!\n");
+                pausa();
+                break;
+
+            case 3:
+                printf("Encerrando...\n");
+                break;
+
+            default:
+                printf("Opcao invalida\n");
+                pausa();
+        }
+    }    
+    while(opcao != 3);    
+
+    return 0;
+
+}
+
+void tela_inicial() {
     printf("====================================\n");
     printf("           QUIZ COM NIVEIS          \n");
     printf("====================================\n");
 
     printf("1 - Jogar\n");
-    printf("2 - Objetivo\n");
+    printf("2 - Objetivo do QUIZ\n");
     printf("3 - Sair\n");
 
     printf("\nEscolha uma opcao: ");
     scanf("%i", &opcao);
+    while (getchar () != '\n');
+    system("cls");
+}    
 
-    switch(opcao)
-    {
-        case 1:
-            printf("\nIniciando...\n");
-            break;
-
-        case 2:
-            printf("\nResponda corretamente para avancar\n");
-            break;
-
-        case 3:
-            printf("\nEncerrando...\n");
-            break;
-
-        default:
-            printf("\nOpcao invalida\n");
-    }
-
-    return 0;
+void pausa() {
+    printf("\nPressione enter para continuar...\n");
+    while (getchar () != '\n');
 }
